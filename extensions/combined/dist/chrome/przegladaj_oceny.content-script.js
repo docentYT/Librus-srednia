bcd2d0e1be14f21b277a07ec3b21e45da35eea6e
docentYT <63965954+docentYT@users.noreply.github.com>
2023-02-03 20:36:48 +0100

    Średnia ocen końcowych i semestralnych, css

--- extensions/combined/dist/chrome/przegladaj_oceny.content-script.js
+++ extensions/combined/dist/chrome/przegladaj_oceny.content-script.js
@@ -121,9 +121,12 @@ function generateTdsIndexes(firstRowLength) {
             "subjectName": 1,
             "gradesFirstTerm": 2,
             "averageFirstTerm": 3,
+            "annualFirst": 4,
             "gradesSecondTerm": 5,
             "averageSecondTerm": 6,
-            "averageYear": 8
+            "annualSecond": 7,
+            "averageYear": 8,
+            "annualYear": 9,
         };
     } else if (firstRowLength == 11) {
         tdsIndexes = {
@@ -131,9 +134,13 @@ function generateTdsIndexes(firstRowLength) {
             "subjectName": 1,
             "gradesFirstTerm": 2,
             "averageFirstTerm": 3,
+            "annualFirstPredirect": 4,
+            "annualFirst": 5,
             "gradesSecondTerm": 6,
             "averageSecondTerm": 7,
-            "averageYear": 9
+            "annualSecond": 8,
+            "averageYear": 9,
+            "annualYear": 10
         };
     } else {
         console.log("Librus Średnia: Ilość kolumn w tabeli z ocenami:", firstRowLength, " proszę zgłosić do developera.")
@@ -154,7 +161,7 @@ function average(gradesList) {
     return (sum/counter).toFixed(2);
 };
 
-function generateFooter(tfoot, subjects) {
+function generateFooter(tfoot, subjects, annuals) {
     function createTd(colspan) {
         let td = document.createElement("td");
         td.setAttribute("colspan", colspan);
@@ -173,17 +180,34 @@ function generateFooter(tfoot, subjects) {
 
     tr.appendChild(createTd(1)); // Spacing
     let totalAverageName = createTd(1);
-    totalAverageName.innerText = "Średnia ze wszystkich ocen";
+    totalAverageName.innerText = "Średnie";
     tr.appendChild(totalAverageName);
     tr.appendChild(createTd(1)); // Spacing
 
     createTdWithAverageFromGradesList(gradesFirst);
-    if (tdsIndexes.tableRowLength == 10) tr.appendChild(createTd(2));   // Spacing
-    else tr.appendChild(createTd(3));                                   // Spacing
+    
+    if (tdsIndexes.tableRowLength == 11) {
+      let annualFirstPredirect = createTd(1);
+      annualFirstPredirect.innerText = annuals["annualFirstPredirect"];
+      tr.appendChild(annualFirstPredirect);
+    };
+
+    let annualFirst = createTd(1);
+    annualFirst.innerText = annuals["annualFirst"];
+    tr.appendChild(annualFirst);
+
+    tr.appendChild(createTd(1));  // Spacing
     createTdWithAverageFromGradesList(gradesSecond);
-    tr.appendChild(createTd(1)); // Spacing
+    
+    let annualSecond = createTd(1);
+    annualSecond.innerText = annuals["annualSecond"];
+    tr.appendChild(annualSecond);
+
     createTdWithAverageFromGradesList(gradesFirst.concat(gradesSecond));
-    tr.appendChild(createTd(1)); // Spacing
+    
+    let annualYear = createTd(1);
+    annualYear.innerText = annuals["annualYear"];
+    tr.appendChild(annualYear);
 };
 
 function updateAverage(tds, subject) {
@@ -211,6 +235,67 @@ function generateSubjectListFromGradesTableBody(tbody, plusValue, minusValue, ty
     return subjectList;
 };
 
+function annualAssements(tbody) {
+    let annuals = {};
+
+    function parseAnnual(tds, tdsIndex) {
+        let grade = tds[tdsIndex];
+        if (!grade) return null;
+        grade = Utils.getTopLevelChildByTagName(grade, "span");
+        if (!grade) return null;
+        grade = Utils.getTopLevelChildByTagName(grade, "a");
+        if (!grade || !grade.textContent) return null;
+        return parseInt(grade.textContent);
+    }
+
+    let annualFirstPredirectCounter = 0;
+    let annualFirstPredirectSum = 0;
+    let annualFirstCounter = 0;
+    let annualFirstSum = 0;
+    let annualSecondCounter = 0;
+    let annualSecondSum = 0;
+    let annualYearCounter = 0;
+    let annualYearSum = 0;
+    for (const subject of tbody.children) {
+        if (subject.hasAttribute("name")) continue;
+        let tds = subject.getElementsByTagName("td");
+        let subjectName = tds[tdsIndexes.subjectName].textContent;
+        if (subjectName.includes("Zachowanie")) continue;
+
+        let annualFirstPredirectGrade = parseAnnual(tds, tdsIndexes.annualFirstPredirect);
+        let annualFirstGrade = parseAnnual(tds, tdsIndexes.annualFirst);
+        let annualSecondGrade = parseAnnual(tds, tdsIndexes.annualSecond);
+        let annualYearGrade = parseAnnual(tds, tdsIndexes.annualYear);
+        if (annualFirstPredirectGrade) {
+            annualFirstPredirectCounter++;
+            annualFirstPredirectSum += annualFirstPredirectGrade;
+        }
+        if (annualFirstGrade) {
+            annualFirstCounter++;
+            annualFirstSum += annualFirstGrade;
+        }
+        if (annualSecondGrade) {
+            annualSecondCounter++;
+            annualSecondSum += annualSecondSum;
+        }
+        if (annualYearGrade) {
+            annualYearCounter++;
+            annualYearSum += annualYearGrade;
+        }
+    }
+    annuals["annualFirstPredirect"] = (annualFirstPredirectSum / annualFirstPredirectCounter).toFixed(2);
+    annuals["annualFirst"] = (annualFirstSum / annualFirstCounter).toFixed(2);
+    annuals["annualSecond"] = (annualSecondSum / annualSecondCounter).toFixed(2);
+    annuals["annualYear"] = (annualYearSum / annualYearCounter).toFixed(2);
+
+    if (isNaN(annuals["annualFirstPredirect"])) annuals["annualFirstPredirect"] = (0).toFixed(2);
+    if (isNaN(annuals["annualFirst"]))          annuals["annualFirst"] = (0).toFixed(2);
+    if (isNaN(annuals["annualSecond"]))         annuals["annualSecond"] = (0).toFixed(2);
+    if (isNaN(annuals["annualYear"]))           annuals["annualYear"] = (0).toFixed(2);
+
+    return annuals;
+}
+
 async function main() {
     let table = document.getElementsByClassName("decorated stretch")[1];
     let tbody = Utils.getTopLevelChildByTagName(table, "tbody");
@@ -219,12 +304,13 @@ async function main() {
 
     let plusValue;
     let minusValue;
-    let tylkoLiczDoSredniej
+    let tylkoLiczDoSredniej;
     await chrome.storage.local.get(["plus"]).then((result) => {plusValue = result.plus ?? 0.5});
     await chrome.storage.local.get(["minus"]).then((result) => {minusValue = result.minus ?? 0.25});
     await chrome.storage.local.get(["tylkoLiczDoSredniej"]).then((result) => {tylkoLiczDoSredniej = result.minus ?? true});
     let subjectList = await generateSubjectListFromGradesTableBody(tbody, plusValue, minusValue, tylkoLiczDoSredniej);
-    generateFooter(Utils.getTopLevelChildByTagName(table, "tfoot"), subjectList);
+    let annuals = annualAssements(tbody);
+    generateFooter(Utils.getTopLevelChildByTagName(table, "tfoot"), subjectList, annuals);
 };
 
 main();
