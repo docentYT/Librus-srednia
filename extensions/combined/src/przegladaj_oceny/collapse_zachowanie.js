bf0834b7b18b2b6ddf40cec772d857178d8c57ce
docentYT <63965954+docentYT@users.noreply.github.com>
2023-09-23 21:29:25 +0200

    use strict

--- extensions/combined/src/przegladaj_oceny/collapse_zachowanie.js
+++ extensions/combined/src/przegladaj_oceny/collapse_zachowanie.js
@@ -1,3 +1,4 @@
+"use strict";
 async function main() {
     const schowajZachowanie = await chrome.runtime.sendMessage({ "message": "getSetting", "key": "schowajZachowanie" });
     if (schowajZachowanie) document.getElementById("przedmioty_zachowanie_node").click();
