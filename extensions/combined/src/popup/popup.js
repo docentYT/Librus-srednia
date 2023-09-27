977149d0478f001e8bb1e886fef76d13097d4bfb
docentYT <63965954+docentYT@users.noreply.github.com>
2023-09-27 21:15:26 +0200

    const $ = document.querySelector; formatting

--- extensions/combined/src/popup/popup.js
+++ extensions/combined/src/popup/popup.js
@@ -1,19 +1,21 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", async function () {
-    const plusInput = document.getElementById("plus");
-    const minusInput = document.getElementById("minus");
-    const tylkoLiczDoSredniej = document.getElementById("tylkoLiczDoSredniej");
-    const schowajZachowanie = document.getElementById("schowajZachowanie");
-    const ignoreCorrectedGrades = document.getElementById("ignoreCorrectedGrades");
-    const nextWeekAtWeekend = document.getElementById("nextWeekAtWeekend");
-    const submitButton = document.getElementById("submit");
+    const $ = document.querySelector.bind(document);
+    
+    const plusInput             = $("#plus");
+    const minusInput            = $("#minus");
+    const tylkoLiczDoSredniej   = $("#tylkoLiczDoSredniej");
+    const schowajZachowanie     = $("#schowajZachowanie");
+    const ignoreCorrectedGrades = $("#ignoreCorrectedGrades");
+    const nextWeekAtWeekend     = $("#nextWeekAtWeekend");
+    const submitButton          = $("#submit");
 
-    plusInput.value = await getData("plus");
-    minusInput.value = await getData("minus");
-    tylkoLiczDoSredniej.checked = await getData("tylkoLiczDoSredniej");
-    schowajZachowanie.checked = await getData("schowajZachowanie");
-    ignoreCorrectedGrades.checked = await getData("ignoreCorrectedGrades");
-    nextWeekAtWeekend.checked = await getData("nextWeekAtWeekend");
+    plusInput.value                 = await getData("plus");
+    minusInput.value                = await getData("minus");
+    tylkoLiczDoSredniej.checked     = await getData("tylkoLiczDoSredniej");
+    schowajZachowanie.checked       = await getData("schowajZachowanie");
+    ignoreCorrectedGrades.checked   = await getData("ignoreCorrectedGrades");
+    nextWeekAtWeekend.checked       = await getData("nextWeekAtWeekend");
 
     submitButton.addEventListener("click", () => {
         saveData(plusInput.value, minusInput.value, tylkoLiczDoSredniej.checked, schowajZachowanie.checked, ignoreCorrectedGrades.checked, nextWeekAtWeekend.checked);
